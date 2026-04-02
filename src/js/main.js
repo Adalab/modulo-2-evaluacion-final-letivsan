8e5f11dd99f744901a6957456498b4a36c37d271
Leti <letivsan@gmail.com>
2026-04-02 14:21:22 +0200

    fetch series from API and display results with fallback image

--- src/js/main.js
+++ src/js/main.js
@@ -6,36 +6,38 @@ const seriesList = document.querySelector('.js_seriesList');
 
 /*SECCIÓN DE DATOS*/
 let seriesData = [];
-// console.log ('Estado inicial de seriesData', seriesData);
 const defaultImage = 'https://placehold.co/210x295/f5f5f5/666666/?text=TV';
 
 /*SECCIÓN DE FUNCIONES*/
-
 function renderSeries () {
     let html = '';
+
     for (const eachSeries of seriesData) {
         const seriesName = eachSeries.show.name;
 
         let seriesImage = defaultImage;
 
-        if (eachSeries.show.image !== null) {
+        if (eachSeries.show.image && eachSeries.show.image.medium) {
             seriesImage = eachSeries.show.image.medium;
         }
-
-        html += `<li class='series__item'>
-        <img class='series__image' src='${seriesImage}' alt='${seriesName}'/>
-        <h2 class= 'series__title'>${seriesName}</h2></li>`;
+    
+        html += `<li class="series__item">
+        <img class="series__image" src="${seriesImage}" alt="${seriesName}"/>
+        <h3 class= "series__title">${seriesName}</h3></li>`;
     }
-
+    
     seriesList.innerHTML = html;
 }
 
 
 function getSeriesFromApi() {
-    console.log('Entro en getSeriesFromApi');
-
-    const searchText = searchInput.value;
+    const searchText = searchInput.value.trim();
     console.log('Texto buscado', searchText);
+
+    if (searchText === '') {
+        seriesList.innerHTML = '';
+        return;
+    }
 
     fetch(`//api.tvmaze.com/search/shows?q=${searchText}`)
         .then((response) => response.json())
@@ -52,15 +54,13 @@ function getSeriesFromApi() {
         });
     }
     
-
 /*SECCIÓN DE FUNCIONES DE EVENTOS*/
 function handleClickSearch (ev) {
     ev.preventDefault ();
     console.log ('Click en buscar');
+
     getSeriesFromApi();
 }
-
-
 
 /*SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA - EJECUCCIÓN*/
 searchButton.addEventListener('click', handleClickSearch);
