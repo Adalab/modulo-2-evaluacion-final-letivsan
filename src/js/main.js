f72c6cf0a80883de46b462eccf7dc33c3d1fff45
Leti <letivsan@gmail.com>
2026-04-03 20:44:03 +0200

    added favorites

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@
 const searchInput = document.querySelector('.js_searchInput');
 const searchButton = document.querySelector('.js_searchButton');
 const seriesList = document.querySelector('.js_seriesList');
-const favoriteList = document.querySelector('.js_favoriteList');
+const favoritesList = document.querySelector('.js_favoritesList');
+const resetFavoritesButton = document.querySelector('.js_resetFavorites');
 
 /*SECCIÓN DE DATOS*/
 let seriesData = [];
@@ -13,42 +14,69 @@ const defaultImage = 'https://placehold.co/210x295/f5f5f5/666666/?text=TV';
 /*SECCIÓN DE FUNCIONES*/
 //Saber si una serie está ya en favoritas
 function isFavorite(seriesId) {
-    const favoriteFound = favoriteSeries.find(
+    const foundFavorite = favoriteSeries.find(
         (eachFavorite) => eachFavorite.id === seriesId
     );
-    return favoriteFound !== undefined;
+
+    console.log('Es fav¿?', seriesId, foundFavorite);
+    return foundFavorite !== undefined;
 }
 
-
 function renderSeries () {
+    console.table(seriesData);
     let html = '';
 
+//recorremos array de series
     for (const eachSeries of seriesData) {
         const seriesId = eachSeries.show.id;
         const seriesName = eachSeries.show.name;
+        const seriesImage = 
+        eachSeries.show.image && eachSeries.show.image.medium 
+        ? eachSeries.show.image.medium : defaultImage;
+     
+        //Comprobar si la serie es favorita
+        const favoriteClass = isFavorite(seriesId) ? 'series__item--favorite' : '';
 
-        let seriesImage = defaultImage;
-
-        if (eachSeries.show.image && eachSeries.show.image.medium) {
-            seriesImage = eachSeries.show.image.medium;
-        }
-
-        let favoriteClass = '';
-
-        if (isFavorite(seriesId)) {
-            favoriteClass = 'series__item--favorite';
-        }
-    
-        html += `<li class="series__item">
+        //Construír el HTML de cada serie
+        html += `<li class="series__item js_seriesItem ${favoriteClass}" data-id="${seriesId}">
         <img class="series__image" src="${seriesImage}" alt="${seriesName}"/>
-        <h3 class= "series__title">${seriesName}</h3></li>`;
+        <h3 class="series__title">${seriesName}</h3></li>`;
     }
     
+    //Pintar todo el HTMl
     seriesList.innerHTML = html;
-
-    addEventListenerToSeries();
+    console.log ('HTML generado para las series', html);
+    //Volver a añadir los eventos a cada serie
+    addEventListenersToSeries();
 }
 
+function renderFavorites() {
+    let html = '';
+
+    if (favoriteSeries.length === 0) {
+        html = '<li class="favorites__empty">Todavía no tienes series favoritas</li>';
+    } else {
+        for (const eachFavorite of favoriteSeries) {
+        const favoriteImage = 
+        eachFavorite.image && eachFavorite.image.medium 
+        ? eachFavorite.image.medium : defaultImage;
+
+         html += `<li class="favorites__item">
+         <img class="favorites__image" src="${favoriteImage}" alt="${eachFavorite.name}"/>
+         <h3 class= "favorites__title">${eachFavorite.name}</h3></li>`;
+        }
+    }
+
+    favoritesList.innerHTML = html;
+}
+
+function addEventListenersToSeries() {
+    const seriesItems = document.querySelectorAll ('.js_seriesItem');
+
+    for (const eachSeriesItem of seriesItems) {
+        eachSeriesItem.addEventListener('click', handleClickSeries);
+    }
+}
 
 function getSeriesFromApi() {
     const searchText = searchInput.value.trim();
@@ -62,18 +90,7 @@ function getSeriesFromApi() {
     fetch(`//api.tvmaze.com/search/shows?q=${searchText}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log('Datos que devuelve la API:', data);
-
-            console.table(
-                data.map((item) => ({
-                    name:item.show.name,
-                    hasImage: item.show.image !== null
-                }))
-            );
-
             seriesData = data;
-            console.log ('seriesData actualizado', seriesData);
-
             renderSeries();
         })
         .catch((error) => {
@@ -85,9 +102,47 @@ function getSeriesFromApi() {
 function handleClickSearch (ev) {
     ev.preventDefault ();
     console.log ('Click en buscar');
-
     getSeriesFromApi();
 }
 
+function handleClickSeries(ev) {
+    const clickedSeriesId = parseInt(ev.currentTarget.dataset.id);
+    console.log('serie clicada en id:', clickedSeriesId);
+
+    const clickedSeries = seriesData.find(
+       (eachSeries) => eachSeries.show.id === clickedSeriesId);
+       console.dir(clickedSeries);
+
+    const favoriteIndex = favoriteSeries.findIndex(
+        (eachFavorite) => eachFavorite.id === clickedSeriesId);
+        console.log('índice en favoritos', favoriteIndex);
+
+    if (favoriteIndex === -1) {
+        console.log('añadiendo a favoritos');
+
+        const favoriteObject = {
+            id: clickedSeries.show.id,
+            name: clickedSeries.show.name,
+            image: clickedSeries.show.image
+        };
+
+        favoriteSeries.push(favoriteObject);
+    } else {
+        favoriteSeries.splice(favoriteIndex, 1);
+    }
+
+    renderFavorites();
+    renderSeries();
+    }
+
+    function handleClickResetFavorites() {
+        favoriteSeries = [];
+        renderFavorites();
+        renderSeries;
+    }
+
 /*SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA - EJECUCCIÓN*/
 searchButton.addEventListener('click', handleClickSearch);
+resetFavoritesButton.addEventListener('click', handleClickResetFavorites);
+renderFavorites();
+
